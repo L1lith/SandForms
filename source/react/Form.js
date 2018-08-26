f58a728cffd3dc1d77a5f273fb65b7da8b077e60
L1lith <lily@lillith.pw>
2018-08-26 16:56:13 -0600

    only create sanitation on text fields

--- source/react/Form.js
+++ source/react/Form.js
@@ -1,56 +1,47 @@
-const { Component, createElement } = require("react");
-const interpretChildren = require("./functions/interpretChildren");
-const React = require("react");
+const { Component, createElement } = require("react")
+const interpretChildren = require("./functions/interpretChildren")
+const React = require("react")
 
 class Form extends Component {
   constructor(props) {
-    super(props);
-    this.inputs = [];
+    super(props)
+    this.inputs = []
     this.children = interpretChildren(this.props.children, input =>
       this.inputs.push(input)
-    );
+    )
   }
   onError(error, data) {
     if (typeof this.props.onError == "function") {
-      this.props.onError(error.message, data);
+      this.props.onError(error.message, data)
     }
   }
   submit() {
-    for (let i = 0; i < this.inputs.length; i++) {
-      const input = this.inputs[i];
-      const error = input.sanitize();
+    for (let i = 0 i < this.inputs.length i++) {
+      const input = this.inputs[i]
+      const error = input.sanitize()
       if (error)
         return this.onError(error, {
           input: input.input,
           value: input.input.value
-        });
+        })
     }
     if (typeof this.props.onSubmit == "function") {
-      const output = {};
+      const output = {}
       this.inputs.forEach(input => {
-        const { name } = input.input;
+        const { name } = input.input
         if (name) {
-          output[name] = input.input.value;
+          output[name] = input.input.value
         } else {
-          if (!output.hasOwnProperty("unknown")) output.unknown = [];
-          output.unknown.push(input.input.value);
+          if (!output.hasOwnProperty("unknown")) output.unknown = []
+          output.unknown.push(input.input.value)
         }
-      });
-      this.props.onSubmit(output);
+      })
+      this.props.onSubmit(output)
     }
   }
   render() {
-    return createElement(
-      "form",
-      {
-        onSubmit: e => {
-          e.preventDefault();
-          this.submit();
-        }
-      },
-      this.children
-    );
+    return createElement("form", {onSubmit: e => {e.preventDefault(); this.submit()}}, this.children)
   }
 }
 
-module.exports = Form;
+module.exports = Form
--- source/react/Input.js
+++ source/react/Input.js
@@ -1,52 +1,51 @@
-const { Component, createElement } = require("react");
-const { sanitize } = require("sandhands");
-let sandhandsOptions = require("sandhands/source/validate/allowedOptions");
+const { Component, createElement } = require("react")
+const { sanitize } = require("sandhands")
+let sandhandsOptions = require("sandhands/source/validate/allowedOptions")
 sandhandsOptions = sandhandsOptions.primitives
   .get(String)
-  .concat(sandhandsOptions.universal);
+  .concat(sandhandsOptions.universal)
 
-const inputOptions = ["onError"];
+const inputOptions = ["onError"]
 
 class Input extends Component {
   constructor(props) {
-    super(props);
-    this.childProps = { ...props };
+    super(props)
+    this.childProps = { ...props }
     // Prevent The Props from being interpreted as options when using vanilla mode
     if (this.childProps.vanilla === true) {
-      this.options = { vanilla: true };
-      delete this.childProps.vanilla;
+      this.options = { vanilla: true }
+      delete this.childProps.vanilla
       return;
     }
     // Interpret the options from the props
-    this.options = {};
-    this.sandhands = { _: String };
+    this.options = {}
+    this.sandhands = { _: String }
     Object.keys(this.props).forEach(prop => {
       if (sandhandsOptions.includes(prop)) {
-        this.sandhands[prop] = this.childProps[prop];
-        delete this.childProps[prop];
+        this.sandhands[prop] = this.childProps[prop]
+        delete this.childProps[prop]
       } else if (inputOptions.includes(prop)) {
-        this.options[prop] = this.childProps[prop];
-        delete this.childProps[prop];
+        this.options[prop] = this.childProps[prop]
+        delete this.childProps[prop]
       }
     });
-    this.useSandhands = Object.keys(this.sandhands).length > 0;
-    if (typeof this.childProps._hook == "function") this.childProps._hook(this);
-    delete this.childProps._hook;
+    this.useSandhands = Object.keys(this.sandhands).length > 0
+    if (typeof this.childProps._hook == "function") this.childProps._hook(this)
+    delete this.childProps._hook
     // Get the input ref
     if (typeof this.childProps.ref == "function") {
-      const propRefHandler = this.childProps.ref;
+      const propRefHandler = this.childProps.ref
       this.childProps.ref = ref => {
-        propRefHandler(ref);
-        this.input = ref;
+        propRefHandler(ref)
+        this.input = ref
       };
     } else {
-      this.childProps.ref = ref => (this.input = ref);
+      this.childProps.ref = ref => (this.input = ref)
     }
   }
   validateOptions() {
-    const { vanilla, validate, onError } = this.options;
-    if (this.options.hasOwnProperty("vanilla") && typeof vanilla != "boolean")
-      throw new Error("Vanilla must be a boolean value");
+    const { vanilla, validate, onError } = this.options
+    if (this.options.hasOwnProperty("vanilla") && typeof vanilla != "boolean") throw new Error("Vanilla must be a boolean value")
     if (
       this.options.hasOwnProperty("validate") &&
       validate !== null &&
@@ -61,25 +60,25 @@ class Input extends Component {
         "Validate must be an object, a function, or an array of functions"
       );
     if (this.options.hasOwnProperty("onError") && typeof onError != "function")
-      throw new Error("onError must be a function");
+      throw new Error("onError must be a function")
   }
   sanitize() {
-    if (this.useSandhands !== true) return;
-    if (!(this.input instanceof HTMLElement))
-      throw new Error("Can't find input ref!");
+    if (this.useSandhands !== true) return
+    if (!(this.input instanceof HTMLElement)) throw new Error("Can't find input ref!");
+    const {value} = this.input
+    if (typeof value != 'string') return // Not A String Input
     try {
-      sanitize(this.input.value, this.sandhands);
+      sanitize(, this.sandhands)
     } catch (error) {
-      if (this.options.onError) this.options.onError(error.message);
-      return error;
+      if (this.options.onError) this.options.onError(error.message)
+      return error
     }
   }
   render() {
     // Render vanilla input if the vanilla prop is passed as true
-    if (this.options.vanilla === true)
-      return createElement("input", this.childProps);
-    return createElement("input", this.childProps);
+    if (this.options.vanilla === true) return createElement("input", this.childProps)
+    return createElement("input", this.childProps)
   }
 }
 
-module.exports = Input;
+module.exports = Input
